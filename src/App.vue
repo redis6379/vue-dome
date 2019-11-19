532c24b8a648a4494454d85494bc7acde521f21e
小新 <zkx6379@163.com>
2019-11-19 18:07:46 +0800

    首页公共模板

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,87 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-row>
+      <el-col :span="4"><!--<div class="grid-content bg-purple"></div>-->
+        <Aside/>
+      </el-col>
+      <el-col :span="20">
+        <el-row>
+          <el-col :span="24">
+            <!--<div class="grid-content bg-purple"></div>-->
+              <Header/>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="24">
+            <div class="grid-content bg-purple"></div>
+          </el-col>
+        </el-row>
+        <div class="grid-content bg-purple"></div>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="24">
+        <div class="grid-content bg-purple"></div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+  import Aside from "./views/aside/Aside";
+  import Header from "./views/header/Header";
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Aside,
+    Header,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body{
+    margin: 0;
+  }
+
+  .gt-aside,.gt-header,.gt-main,.gt-footer{
+    background-color: #B3C0D1;
+  }
+
+  .gt-aside{
+    background-color: antiquewhite;
+    height: 100%;
+  }
+
+  .gt-header{
+    background-color: darkgray;
+    height: 70px;
+  }
+  .gt-main{
+    background-color: aquamarine;
+    height: 130px;
+  }
+
+  .gt-footer{
+    background-color: #B3C0D1;
+    height: 40px;
+  }
+
+
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-  <el-container>
+<!--  <el-container>
     <el-header>https://element.eleme.cn/#/zh-CN</el-header>
     <el-main>Main</el-main>
   </el-container>
@@ -38,19 +38,21 @@
   <el-container>
     <el-aside width="200px"><div>
       <el-link icon="el-icon-edit">编辑</el-link>
-      <el-link><i class="el-icon-view el-icon--right">查看</i> </el-link>
+      <el-link><i class="el-icon-view el-icon&#45;&#45;right">查看</i> </el-link>
     </div></el-aside>
     <el-container>
       <el-header>Header</el-header>
       <el-main>Main</el-main>
     </el-container>
-  </el-container>
+  </el-container>-->
 
   <el-container>
     <el-aside width="200px">Aside</el-aside>
     <el-container>
       <el-header>Header</el-header>
-      <el-main>Main</el-main>
+      <el-main>
+        Main
+      </el-main>
       <el-footer>Footer</el-footer>
     </el-container>
   </el-container>
@@ -97,7 +99,6 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
   .el-header, .el-footer {
     background-color: #B3C0D1;
@@ -124,12 +125,5 @@ export default {
     margin-bottom: 40px;
   }
 
-  .el-container:nth-child(5) .el-aside,
-  .el-container:nth-child(6) .el-aside {
-    line-height: 260px;
-  }
 
-  .el-container:nth-child(7) .el-aside {
-    line-height: 320px;
-  }
 </style>
